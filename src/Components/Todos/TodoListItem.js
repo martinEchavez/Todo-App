ed827f014e6f8ac49dcbd337101cb0c69dd34b6c
martinEchavez <martinechavez1994@gmail.com>
2020-08-11 11:45:57 -0500

    Iconos agregados

--- src/Components/Todos/TodoListItem.js
+++ src/Components/Todos/TodoListItem.js
@@ -1,21 +1,27 @@
 import React from 'react';
 
-const TodoListItem = ({ todo, handleDelete, handleToggle}) => {
+const TodoListItem = ({ todo, handleDelete, handleToggle }) => {
     return (
         <div className="card mt-2" key={todo.id}>
             <div className="card-header d-flex justify-content-between">
                 <h4>{todo.title}</h4>
-                <button
-                    className="btn btn-danger offset"
+                <a
+                    style={{color: 'Tomato'}}
+                    href="#"
                     onClick={() => handleDelete(todo.id)}
-                >X</button>
+                >
+                    <i className="fas fa-trash-alt"></i>
+                </a>
             </div>
             <div className="card-body d-flex justify-content-between">
                 <p className={`${todo.done && 'complete'}`}> {todo.description} </p>
-                <button
-                    className="btn btn-success"
+                <a
+                    style={{color: 'green'}}
+                    href="#"
                     onClick={() => handleToggle(todo.id)}
-                >Y</button>
+                >
+                    <i className="fas fa-check-square"></i>
+                </a>
             </div>
         </div>
     );
