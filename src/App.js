7ad0ec9374bc1af5b1f0deef5681a42c1c346e33
martinEchavez <martinechavez1994@gmail.com>
2020-08-11 11:28:06 -0500

    Rutas agregadas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar'
 import Dashboard from './Components/Todos/Dashboard';
 
 const App = () => {
 
   return (
-    <>
+    <Router>
+      {/* Navegación */}
       <Navbar />
-      <Dashboard />
-    </>
+      {/* Componentes por URL */}
+      <Route path="/" exact component={Dashboard} />
+    </Router>
   );
 };
 
