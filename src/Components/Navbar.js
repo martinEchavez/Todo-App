31fd1641091026751a189336e32450a24c4dddc6
martinEchavez <martinechavez1994@gmail.com>
2020-08-11 11:59:12 -0500

    Logo actualizado

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link className="navbar-brand" to="/">
-                <img src="https://raw.githubusercontent.com/martinEchavez/Todo-App/master/src/img/Wombat.png" width="30" height="30" className="d-inline-block align-top" alt="Wombats" loading="lazy" />
+                <img src="https://raw.githubusercontent.com/martinEchavez/Todo-App/ed827f014e6f8ac49dcbd337101cb0c69dd34b6c/src/img/Wombat.svg" width="30" height="30" className="d-inline-block align-top" alt="Wombats" loading="lazy" />
                 Wombats
             </Link>
         </nav>
