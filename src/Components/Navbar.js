fdc6d9d1ec78bdcdf39c10c1f5f4c9493d0be72b
martinEchavez <martinechavez1994@gmail.com>
2020-08-11 12:06:00 -0500

    Etiquetas a remplazadas

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,8 +5,8 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link className="navbar-brand" to="/">
-                <img src="https://raw.githubusercontent.com/martinEchavez/Todo-App/ed827f014e6f8ac49dcbd337101cb0c69dd34b6c/src/img/Wombat.svg" width="30" height="30" className="d-inline-block align-top" alt="Wombats" loading="lazy" />
-                Wombats
+                <img src="https://raw.githubusercontent.com/martinEchavez/Todo-App/master/src/img/logo.jpg" width="30" height="30" className="d-inline-block align-top" alt="Wombats" loading="lazy" />
+                {' '}Wombats
             </Link>
         </nav>
     );
--- src/Components/Todos/TodoListItem.js
+++ src/Components/Todos/TodoListItem.js
@@ -1,27 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TodoListItem = ({ todo, handleDelete, handleToggle }) => {
     return (
         <div className="card mt-2" key={todo.id}>
             <div className="card-header d-flex justify-content-between">
                 <h4>{todo.title}</h4>
-                <a
+                <Link
                     style={{color: 'Tomato'}}
-                    href="#"
+                    to="#"
                     onClick={() => handleDelete(todo.id)}
                 >
                     <i className="fas fa-trash-alt"></i>
-                </a>
+                </Link>
             </div>
             <div className="card-body d-flex justify-content-between">
                 <p className={`${todo.done && 'complete'}`}> {todo.description} </p>
-                <a
+                <Link
                     style={{color: 'green'}}
-                    href="#"
+                    to="#"
                     onClick={() => handleToggle(todo.id)}
                 >
                     <i className="fas fa-check-square"></i>
-                </a>
+                </Link>
             </div>
         </div>
     );
